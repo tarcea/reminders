76a2f8d4369733dc70fe4088243ef35ff4327b89
tarcea <tarcea@gmail.com>
2021-12-27 14:11:04 +0200

    add todo for acertain list done in frontend

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState, FormEvent } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { io } from 'socket.io-client';
-import { getListById } from '../Api';
+import { addTodo, getListById } from '../Api';
 
 const List: FC<{ currentId: string }> = ({ currentId }) => {
+  const initialState: Omit<ITodo, '_id' | 'done'> = { name: '', description: '', cost: 0 };
+  const [formData, setFormData] = useState(initialState);
   const [message, setMessage] = useState<String>('nothing to show');
   const [list, setList] = useState<IList>();
   const { listId } = useParams();
@@ -19,8 +21,8 @@ const List: FC<{ currentId: string }> = ({ currentId }) => {
   };
 
   useEffect(() => {
-    const socket = io('http://localhost:3001'); // DEV
-    // const socket = io(process.env.REACT_APP_API_URL!); // PROD
+    // const socket = io('http://localhost:3001'); // DEV
+    const socket = io(process.env.REACT_APP_API_URL!); // PROD
     socket.on('connect', () => {
       setMessage(`You connected with id: ${socket.id}`)
       socket.emit('fetch_data', currentId)
@@ -40,10 +42,46 @@ const List: FC<{ currentId: string }> = ({ currentId }) => {
     }
   }, [listId]);
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await addTodo({ ...formData, _id: '', done: false }, listId!)
+    setFormData(initialState);
+  };
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.currentTarget.id]: e.currentTarget.value,
+    });
+  };
+
   return (
     <div>
       <p>{list?.todos?.length} -- {list?.name}</p>
       <p>{message}</p>
+      <form onSubmit={handleSubmit}>
+        <label>name</label>
+        <input
+          type="text"
+          id="name"
+          value={formData.name}
+          onChange={handleChange} />
+        <label>description</label>
+        <input
+          type="text"
+          id="description"
+          value={formData.description}
+          onChange={handleChange} />
+        <label>price</label>
+        <input
+          type="number"
+          id="cost"
+          value={formData.cost}
+          onChange={handleChange} />
+        <input
+          type="submit"
+          value="add todo" />
+      </form>
     </div>
   );
 };
